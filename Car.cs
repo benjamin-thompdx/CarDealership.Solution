0cabcab6435f8fe4bbfdf313fc97a708030fbb5b
Eric Settels and Keturah Howard <eric+keturah@>
2020-02-25 08:34:53 -0800

    add functionality to spit out descriptions and  say when you didnt get any results

--- Car.cs
+++ Car.cs
@@ -6,41 +6,50 @@ public class Car
   public string MakeModel;
   public int Price;
   public int Miles;
+  public string Description;
 
-  public Car(string makeModel, int price, int miles)
+  public Car(string makeModel, int price, int miles, string description)
   {
     MakeModel = makeModel;
     Price = price;
     Miles = miles;
+    Description = description;
   }
 
   public bool WorthBuying(int maxPrice)
   {
     return (Price < maxPrice);
   }
+  public bool GoodMilage(int maxMilage)
+  {
+    return (Miles < maxMilage);
+  }
 }
 public class Program
 {
   public static void Main()
   {
-    Car volkswagen = new Car("1974 Volkswagen Thing", 1100, 368792);
-    Car yugo = new Car("1980 Yugo Koral", 700, 56000);
-    Car ford = new Car("1988 Ford Country Squire", 1400, 239001);
-    Car amc = new Car("1976 AMC Pacer", 400, 198000);
-    Car mazda = new Car("2002 Mazda 626", 2000, 110000);
+    Car volkswagen = new Car("1974 Volkswagen Thing", 1100, 368792, "wow! its groovay baby! ");
+    Car yugo = new Car("1980 Yugo Koral", 700, 56000, "sexiest cerial box you can drive!");
+    Car ford = new Car("1988 Ford Country Squire", 1400, 239001, "'MerIca");
+    Car amc = new Car("1976 AMC Pacer", 400, 198000, "ummmm");
+    Car mazda = new Car("2002 Mazda 626", 2000, 110000, "my first car");
 
-    List<Car> Cars = new List<Car>() {volkswagen, yugo, ford, amc};
+    List<Car> Cars = new List<Car>() {volkswagen, yugo, ford, amc, mazda};
 
-    Console.WriteLine("Enter maximum price: ");
+    Console.Write("Enter maximum price: ");
     string stringMaxPrice = Console.ReadLine();
     int maxPrice = int.Parse(stringMaxPrice);
+    Console.Write("Enter maximum Milage: ");
+    string stringMaxMilage = Console.ReadLine();
+    int maxMilage = int.Parse(stringMaxMilage);
 
     List<Car> CarsMatchingSearch = new List<Car>(0);
 
     // compares all cars to price range, filtering out cars outside it
     foreach(Car automobile in Cars)
     {
-      if (automobile.WorthBuying(maxPrice) == true)
+      if (automobile.WorthBuying(maxPrice) && automobile.GoodMilage(maxMilage))
       {
         CarsMatchingSearch.Add(automobile);
       }
@@ -49,7 +58,13 @@ public class Program
     //prints out cars within price range
     foreach(Car automobile in CarsMatchingSearch)
     {
-      Console.WriteLine(automobile.MakeModel);
+      Console.Write(automobile.MakeModel + ":");
+      Console.WriteLine(" " + automobile.Description);
+    }
+
+    if (CarsMatchingSearch.Count == 0)
+    {
+      Console.WriteLine("Oops your a picky nicky");
     }
   }
 }
